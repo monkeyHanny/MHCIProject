9e1eb6c469575ba1428f6ecbfd44dbe3161b2f10
Hanny <4690240+monkeyHanny@users.noreply.github.com>
2018-02-22 18:19:50 +0800

    Initial commit

---
+++ app/src/main/java/com/mhci/ax/InitActivity.java
@@ -0,0 +1,16 @@
+package com.mhci.ax;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by monkeyhanny on 22/2/2018.
+ */
+
+public class InitActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView();
+    }
+}
